8ddc14fbd460ce38005a9caa1ae4e2888ed547d6
apm1005 <apm1005@alu.ubu.es>
2019-03-21 14:07:46 +0100

    Added p5 test

--- p5/solution.py
+++ p5/solution.py
@@ -153,5 +153,100 @@ class TestParticion(unittest.TestCase):
                 s.remove(b)
                 self.assertEqual(p.numero(), n)
 
+
+def arbol_extendido_kruskal(grafo): #TODO
+    """
+    Dado un grafo devuelve otro grafo con el árbol expandido mínimo,
+    utilizando el algoritmo de Kruskal.
+    Los grafos son diccionario donde las claves son arcos (pares de nodos) y los
+    valores son el peso de los arcos.
+    """
+    nodos = []
+    for n1,n2 in grafo:
+        if n1 not in grafo:
+            nodos.append(n1)
+        if n2 not in grafo:
+            nodos.append(n2)
+
+    p = Particion(nodos)
+    arbol_extendido = {}
+    
+    for arco in grafo:
+        n1, n2 = arco
+        peso = grafo[arco]
+        if p[n1] != p[n2]:
+            p.une(n1,n2)
+            arbol_extendido[(n1,n2)] = peso
+    
+    return arbol_extendido
+        
+
+
+class TestArbolExtendidoKruskal(unittest.TestCase):
+    
+    def test_6_nodos_9_arcos(self):
+            
+        g = {("a", "b"): 13, 
+             ("a", "c"): 8,
+             ("a", "d"): 1,
+             ("b", "c"): 15,
+             ("c", "d"): 5,
+             ("c", "e"): 3,
+             ("d", "e"): 4,
+             ("d", "f"): 5,
+             ("e", "f"): 2}
+    
+        t = {("a", "b"): 13, 
+             ("a", "d"): 1,
+             ("c", "e"): 3,
+             ("d", "e"): 4,
+             ("e", "f"): 2}
+        
+        self.assertEqual(arbol_extendido_kruskal(g), t)
+    
+    def test_7_nodos_12_arcos(self):
+    
+        g = {("a", "b"): 2, 
+             ("a", "c"): 4,
+             ("a", "d"): 1,
+             ("b", "d"): 3,
+             ("b", "e"): 10,
+             ("c", "d"): 2,
+             ("c", "f"): 5,
+             ("d", "e"): 7,
+             ("d", "f"): 8,
+             ("d", "g"): 4,
+             ("e", "g"): 6,
+             ("f", "g"): 1}
+        
+        t = {("a", "b"): 2, 
+             ("a", "d"): 1,
+             ("c", "d"): 2,
+             ("d", "g"): 4,
+             ("e", "g"): 6,
+             ("f", "g"): 1}
+    
+        self.assertEqual(arbol_extendido_kruskal(g), t)
+        
+    def test_grafo_aleatorio(self, n=10, repeticiones=10, semilla=1):
+        """Tests con grafos completos aleatorios de n nodos"""
+        
+        random.seed(semilla)
+        for _ in range(repeticiones):
+            
+            # Creamos el grafo
+            g = {(i, j): n + 1 for i in range(n - 1) for j in range(i + 1, n)}
+            for i in range(1, n + 1):
+                g[random.randint(0, i - 1), i] = i
+            t = arbol_extendido_kruskal(g)
+            
+            # Comprobamos que los arcos del árbol están en el grado y 
+            # que el peso total es el esperado
+            total = 0
+            for arco, peso in t.items():
+                self.assertEqual(peso, g[arco])
+                total += peso
+            self.assertEqual(total, n * (n + 1) / 2)
+
 if __name__ == '__main__':
     unittest.main(argv=['first-arg-is-ignored'], exit=False)
